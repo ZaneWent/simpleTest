de3e799462e2fc427a309abe3586a57b7ad6d301
idev <idev001@idevtekiMac-mini.local>
2015-01-04 15:50:18 +0800

    Initial Commit

---
+++ simpleTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  simpleTest
+//
+//  Created by idev on 15/1/4.
+//  Copyright (c) 2015年 idev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ simpleTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  simpleTest
+//
+//  Created by idev on 15/1/4.
+//  Copyright (c) 2015年 idev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
